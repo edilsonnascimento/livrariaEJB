c3602fdc4e30a0fe5fd3e4b6ebec855b4730bed2
Edilson do Nascimento <edilsonjoper@gmail.com>
2019-02-14 09:17:31 -0200

    02 - Ciclo de vida dos Session Beans: teste, Callbacks, Thread Safety e @Singleton

--- src/br/com/caelum/livraria/dao/AutorDao.java
+++ src/br/com/caelum/livraria/dao/AutorDao.java
@@ -4,14 +4,31 @@ import java.util.List;
 
 import br.com.caelum.livraria.modelo.Autor;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 @Stateless
 public class AutorDao {
 
-    private Banco banco = new Banco();
+    @Inject
+	private Banco banco;
 
+    
+    public void aposCriacao() {
+    	System.out.println("[INFO] AutorDAO foi criado...");
+    }
     public void salva(Autor autor) {
+    	
+    	System.out.println("[INFO} Salvando Autor: " + autor.getNome());
+    	
+    	try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+    	
         banco.save(autor);
+        
+        System.out.println("[INFO] Salvo Autor: " + autor.getNome());
     }
 
     public List<Autor> todosAutores() {
--- src/br/com/caelum/livraria/dao/LivroDao.java
+++ src/br/com/caelum/livraria/dao/LivroDao.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import br.com.caelum.livraria.modelo.Livro;
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 
 @Stateless
 public class LivroDao {
 
-    private Banco banco = new Banco();
+    @Inject
+	private Banco banco;
 
     public void salva(Livro livro) {
         banco.save(livro);
